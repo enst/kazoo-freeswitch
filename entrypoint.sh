33b092ec0c7ff020dfe03657f709b2b1524ed235
Bing Li <enst.bupt@gmail.com>
2015-08-07 19:38:48 -0400

    sudo -u freeswitch freeswitch -nc -nonat

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 sed -i "/cookie/s/change_me\" \/>-->/$COOKIE\" \/>/" /etc/kazoo/freeswitch/autoload_configs/kazoo.conf.xml
 
-service freeswitch start
+#service freeswitch start
+sudo -u freeswitch freeswitch -nc -nonat
 
 exec /bin/bash "$@"
 
